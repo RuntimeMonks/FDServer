abeca7bed82d7c4afd8047e8c28c2652bb9a0d95
SadikBasha <sadikshaikh309@gmail.com>
2019-11-21 19:11:52 +0530

    Initial Commit
    Added winner api's

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const express = require('express')
 require('./utils/db/mongoose')
 const frouter = require('./routers/feedback')
 var cors = require('cors')
+const winnerRouters = require('./routers/winner')
 
 const app = express()
 const portNo = 3000
 app.use(express.json())
 app.use(cors())
 app.use(frouter)
+app.use(winnerRouters)
 
 app.listen(portNo, () => {
     console.log("Server is ON : " + portNo)
