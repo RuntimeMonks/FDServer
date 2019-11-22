aa0a707835db1ef496e183ee968293d9a8561760
Anand Lok <imanandlok@gmail.com>
2019-11-22 13:35:09 +0530

    akshay commit

--- src/core/user/user.js
+++ src/core/user/user.js
@@ -2,76 +2,80 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken')
-const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        unique:true,
-        required:true,
-        validate(value){
-            if(!validator.isEmail(value)){
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
                 throw new Error('Please Enter a valid Email')
             }
         }
     },
-    eid:{
-        type:Number,
-        required:true,
-        unique:true
+    eid: {
+        type: Number,
+        required: true,
+        unique: true
     },
-    gender:{
-        type:String,
-        required:true
+    gender: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:6,
-        validate(value){
-            if(value.toLowerCase().includes('password')){
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 6,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
                 throw new Error('you cannot include "password" in password')
             }
         }
     },
-  
-       token:{
-           type: String,
+    token: {
+        type: String,
         //    required: true
-       }
-   
+    }
+
 })
-userSchema.methods.generateAuthToken = async function (){
+userSchema.methods.generateAuthToken = async function() {
     // const user = this
-    this.token = jwt.sign({ id:user.eid.toString() },'thisismycourse')
+    console.log("hi", this);
+    this.token = jwt.sign({
+        eid: this.eid.toString()
+    }, 'thisismycourse')
     console.log(this)
     await this.save()
-    return token
+    return this.token
 }
 
-userSchema.statics.findByCredentials = async (email,password) =>{
-    const user = await User.findOne({ email })
-    if(!user){
-        throw new error ('Unable to login')
-    }  
-    const isMatch = await bcrypt.compare(password,user.password)
-    if(!isMatch){
+userSchema.statics.findByCredentials = async(email, password) => {
+    const user = await User.findOne({
+        email
+    })
+    if (!user) {
+        throw new error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
         throw new Error('unable to login')
     }
     return user
 }
-userSchema.pre('save',async function(next){
+userSchema.pre('save', async function(next) {
     const user = this
-    if (user.isModified('password')){
-        user.password = await bcrypt.hash(user.password,8)
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
     }
 
     // console.log("just before saving")
     next()
 })
 
-const User= mongoose.model('User',userSchema)
+const User = mongoose.model('User', userSchema)
 module.exports = User
