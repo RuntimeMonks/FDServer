e99559c303a0fb89f53482293b1bba974b6fc889
Anand Lok <imanandlok@gmail.com>
2019-11-22 14:56:11 +0530

    Update user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -18,7 +18,10 @@ router.post('/login', async(req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({ user, token })
+        res.send({
+            user,
+            token
+        })
     } catch (e) {
         res.status(400).send()
     }
