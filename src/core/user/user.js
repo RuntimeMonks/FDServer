1565d3e17e610d69d883ab01ad1fb6a9ab7a74f5
akshay <akki24.akshay@gmail.com>
2019-11-22 13:05:20 +0530

    AG 22

---
+++ src/core/user/user.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken')
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Please Enter a valid Email')
+            }
+        }
+    },
+    eid:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    gender:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:6,
+        validate(value){
+            if(value.toLowerCase().includes('password')){
+                throw new Error('you cannot include "password" in password')
+            }
+        }
+    },
+  
+       token:{
+           type: String,
+        //    required: true
+       }
+   
+})
+userSchema.methods.generateAuthToken = async function (){
+    // const user = this
+    this.token = jwt.sign({ id:user.eid.toString() },'thisismycourse')
+    
+    await this.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email,password) =>{
+    const user = await User.findOne({ email })
+    if(!user){
+        throw new error ('Unable to login')
+    }  
+    const isMatch = await bcrypt.compare(password,user.password)
+    if(!isMatch){
+        throw new Error('unable to login')
+    }
+    return user
+}
+userSchema.pre('save',async function(next){
+    const user = this
+    if (user.isModified('password')){
+        user.password = await bcrypt.hash(user.password,8)
+    }
+
+    // console.log("just before saving")
+    next()
+})
+
+const User= mongoose.model('User',userSchema)
+module.exports = User
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 require('./utils/db/mongoose')
 const frouter = require('./routers/feedback')
+const urouter = require('./routers/user')
 var cors = require('cors')
 
 const app = express()
 const portNo = 3000
 app.use(express.json())
 app.use(cors())
+app.use(urouter)
 app.use(frouter)
 
 app.listen(portNo, () => {
