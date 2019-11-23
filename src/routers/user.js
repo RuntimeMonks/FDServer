743ec518fcc0a32ddeced00354593766044c9b95
Anand Lok <imanandlok@gmail.com>
2019-11-23 16:02:24 +0530

    Update user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -30,4 +30,33 @@ router.get('/login', async(req, res) => {
         res.status(400).send()
     }
 })
+
+router.post('/logout', async(req, res) => {
+    try {
+        req.user.token = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/update/me', async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'eid', 'gender', 'utype', 'event']
+    const validateUpdates = updates.every((updates) => allowedUpdates.includes(updates))
+        // console.log(validateUpdates)
+    if (!validateUpdates) {
+        return res.status(400).send({ error: 'Invalid Update' })
+    }
+    try {
+        updates.forEach(update => req.users[update] = req.body[update])
+        await req.users.save()
+
+        res.status(200).send(req.users)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 module.exports = router
