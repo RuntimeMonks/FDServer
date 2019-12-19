13fcbd94210e22685b0c8d47fd2db33f897e94dd
Anand Lok <imanandlok@gmail.com>
2019-12-20 00:29:46 +0530

    Update mongoose.js

--- src/utils/db/mongoose.js
+++ src/utils/db/mongoose.js
@@ -1,6 +1,14 @@
+// const mongoose = require('mongoose')
+
+// mongoose.connect('mongodb://localhost:27017/FDServer', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: true
+// })
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/FDServer', {
+mongoose.connect('mongodb+srv://anandlok:Punita@2@cluster0-oao9h.mongodb.net/FDServer?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
